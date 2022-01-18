97777d60fad36da3d20e02f1cd9d76840c0bb358
David Chamont <chamont@in2p3.fr>
2022-01-18 17:00:12 +0100

    Enum new class.

--- glossary.cpp
+++ glossary.cpp
@@ -1,19 +1,28 @@
 
 #include "glossary.h"
-#include <string_view>
-#include <string>
-#include <sstream>
 
+// Glossary
 using Columns = Glossary<struct ColumnsFoo> ;
 Columns const COLUMNS("couleur1|color1;couleur2|color2") ;
-Columns::Id C1 = COLUMNS.id("couleur1") ;
-Columns::Id C2 = COLUMNS.id("color2") ;
+
+// Enum
+using FiliereEnum = Enum<struct FiliereTag> ;
+template<> FiliereEnum::EnumGlossary FiliereEnum::glo__ {} ;
+FiliereEnum const FILIERE_MP { "MP", true } ;
+FiliereEnum const FILIERE_PC { "PC", true } ;
 
 int main()
  {
   auto c1 = COLUMNS.id("COLOR1"), c2 = COLUMNS.id("Couleur2") ;
-  std::cout<<c1<<" "<<C2<<std::endl ;
-  std::cout<<COLUMNS.str(C1)<<" "<<COLUMNS.str(c2)<<std::endl ;
+  std::cout<<c1<<" "<<c2<<std::endl ;
+  std::cout<<COLUMNS.str(c1)<<" "<<COLUMNS.str(c2)<<std::endl ;
+
+  FiliereEnum f1, f2, f3 ;
+  std::istringstream iss ("mp Pc PSI") ;
+  iss>>f1>>f2 ; //>>f3 ;
+  std::cout<<f1<<" "<<f2<<std::endl ;
+  std::cout<<FILIERE_MP<<" "<<FILIERE_PC<<std::endl ;
+  std::cout<<(f1==FILIERE_MP)<<" "<<(FILIERE_PC==f2)<<std::endl ;
 
   return 0 ;
  }
--- glossary.h
+++ glossary.h
@@ -1,44 +1,75 @@
 
+
 #ifndef GLOSSARY_H
 #define GLOSSARY_H
 
-#include "strong_int.h"
-#include "frequent_strings.h"
 #include <string>
 #include <string_view>
 #include <vector>
-#include <set>
-#include <unordered_map>
 #include <iostream>
+#include <sstream>
+#include <utility>
+
 
 //===================================================
-// Interface
+// Glossaire
 //===================================================
 
 template <typename TagType>
 class Glossary
-{
+ {
 
   public:
+ 
+    using Collection = std::vector<std::vector<std::string>> ;
+    using Id = Collection::size_type ;
   
-    using VString = std::vector<std::string>;
-    using VCItr = VString::const_iterator;
-    using VVString = std::vector<VString>;
-    using Id = VVString::size_type;
-  
-    explicit Glossary(std::string_view);
-    Id id(std::string_view) const;
-    std::string const & str(Id) const;
-    Id size() const;
-    
+    explicit Glossary( std::string_view = "" ) ;
+    Id add( std::string_view ) ;
+    Id id( std::string_view ) const ;
+    Id size() const { return data_.size() ; }
+    std::string const & str( Id id ) const { return (*(data_[id].begin())); }
+
   private:
   
-    std::vector<std::vector<std::string>> data_;
+    Collection data_ ;
 
-};
+ } ;
+
 
 //===================================================
-// Implementation
+// Enum
+//===================================================
+
+template <typename TagType>
+class Enum
+ {
+  public :
+
+    using EnumGlossary = Glossary<TagType> ;
+
+    Enum() : id_(glo__.size()) {}
+    Enum( std::string_view sv, bool add = false )
+     : id_(add?glo__.add(sv):glo__.id(sv)) {}
+
+    friend bool operator==( Enum lhs, Enum rhs ) { return (lhs.id_==rhs.id_) ; }
+    friend bool operator!=( Enum lhs, Enum rhs ) { return (lhs.id_!=rhs.id_) ; }
+
+    friend std::ostream & operator<<( std::ostream & os, Enum<TagType> e )
+     { return (os<<e.glo__.str(e.id_)) ; }
+    friend std::istream & operator>>( std::istream & is, Enum<TagType> & e )
+     { std::string str ; if (is>>str) e.id_ = e.glo__.id(str) ; return is ; }
+
+  private :
+
+    EnumGlossary::Id id_ ;
+    static EnumGlossary glo__ ;
+ } ;
+
+
+
+//===================================================
+// External implementations
 //===================================================
 
 static void glossary_lower(std::string & str)
@@ -49,7 +80,7 @@ static void glossary_lower(std::string & str)
 }
 
 template <typename TagType>
-Glossary<TagType>::Glossary(std::string_view terms_sv)
+Glossary<TagType>::Glossary( std::string_view terms_sv )
 {
   std::istringstream terms_iss{std::string{terms_sv}};
   std::string synonyms_str;
@@ -68,32 +99,39 @@ Glossary<TagType>::Glossary(std::string_view terms_sv)
 }
 
 template <typename TagType>
-Glossary<TagType>::Id Glossary<TagType>::id(std::string_view term_sv) const
-{
+Glossary<TagType>::Id Glossary<TagType>::add( std::string_view terms_sv )
+ {
+  std::istringstream terms_iss{std::string{terms_sv}} ;
+  std::vector<std::string> terms ;
+  std::string term ;
+  while (std::getline(terms_iss,term,'|'))
+   {
+    glossary_lower(term) ;
+    terms.push_back(std::move(term)) ;
+   }
+  data_.push_back(std::move(terms)) ;
+  return (data_.size()-1) ;
+ }
+
+template <typename TagType>
+Glossary<TagType>::Id Glossary<TagType>::id( std::string_view term_sv ) const
+ {
   std::string term_str(term_sv);
   glossary_lower(term_str);
   Id res = 0;
   while (res < data_.size())
-  {
+   {
     auto const &words = data_[res];
     if (find(words.begin(), words.end(), term_str) != words.end())
-    {
-      return res;
-    }
+     { return res ; }
     else
-    {
-      ++res;
-    }
-  }
-  throw std::runtime_error("Unknown term");
-}
+     { ++res ; }
+   }
+  std::ostringstream oss ;
+  oss<<"Unknown term: "<<term_sv ;
+  throw std::runtime_error(oss.str()) ;
+ }
 
-template <typename TagType>
-std::string const & Glossary<TagType>::str(Id term_id) const
- { return (*(data_[term_id].begin())); }
-
-template <typename TagType>
-Glossary<TagType>::Id Glossary<TagType>::size() const
-  { return data_.size() ; }
 
 #endif
+
